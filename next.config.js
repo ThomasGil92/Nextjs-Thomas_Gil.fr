30e39bad3e3640ac5f2c70d81bd01f0b52e8620d
Thomas Gil <tgil849@gmail.com>
2021-04-14 17:37:24 +0200

    cors policy

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ require("dotenv").config();
 module.exports = {
   env: {
     REST_API:process.env.REST_API
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
+      },
+    ]
+  },
 };
