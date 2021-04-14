27e1835a525c77938ced71db0b45687aa56417e3
Thomas Gil <tgil849@gmail.com>
2021-04-14 16:38:37 +0200

    deploy

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk");
 const { contactForm } = require("../../utils/emailTemplates");
 
 AWS.config.update({
-  accessKeyId: process.env.AMAZON_KEY_ID,
+  accessKeyId: process.env.AMAZON_ACCESS_KEY_ID,
   secretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY,
   region: process.env.AMAZON_REGION,
 });
